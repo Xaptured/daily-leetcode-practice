039561696cb48274f245a89b67aa91e5eb9eea08
Jack <jk19011999@gmail.com>
2025-05-13 10:20:50 +0530

    Day-12

---
+++ random_ques/FibonacciNumber_509.java
@@ -0,0 +1,9 @@
+package random_ques;
+
+public class FibonacciNumber_509 {
+    public int fib(int n) {
+        if (n==0) return 0;
+        if (n==1) return 1;
+        return fib(n-1) + fib(n-2);
+    }
+}
